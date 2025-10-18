f6591e373751bdd0a402ce1f686cb9f874922f93
Domi <donniesan76@gmail.com>
2025-10-18 10:48:53 +0700

    Add more UI and fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-import  LandingPage from './pages/LandingPage.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LandingPage from "./pages/LandingPage.jsx";
 
 function App() {
-
   return (
     <>
       <LandingPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,32 +1,111 @@
-import React from 'react';
-import "./LandingPage.css"; 
+import { useState } from "react";
+import ResultCard from "../components/ResultCard";
+import "./LandingPage.css";
 
 function LandingPage() {
-  return ( 
-    <div className="main-page">
+  const [form, setForm] = useState({
+    name: "John Doe",
+    image: null,
+  });
 
-      <div className="patient-input">
-        <h1>
-          SELAMAT DATANG
-        </h1>
+  const [results, setResults] = useState([]);
 
-        <div>
-          <p>Patient Name</p>
-          <input placeholder="Insert Name" />
+  const handleChange = (e) => {
+    setForm({ ...form, name: e.target.value });
+  };
+
+  const handleImageChange = (e) => {
+    setForm({ ...form, image: e.target.files[0] });
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("file", form.image);
+    formData.append("name", form.name);
+    formData.append("age", form.age);
+    formData.append("height", form.height);
+
+    try {
+      const res = await fetch("http://127.0.0.1:8000/predict", {
+        method: "POST",
+        body: formData,
+      });
+
+      const resJson = await res.json();
+
+      if (res.ok) {
+        const newResult = {
+          name: form.name,
+          testResult: resJson.prediction || "Unknown",
+          confidence: resJson.confidence || "N/A",
+        };
+        setResults((prev) => [...prev, newResult]);
+      } else {
+        alert("Upload failed");
+      }
+    } catch (err) {
+      console.error("Error uploading: ", err);
+    }
+  };
+
+  return (
+    <div className="patient">
+      {/* FORM */}
+      <div className="patient__top-container">
+        <div className="patient__left-container">
+          {/* NAME INPUT */}
+          <div className="patient__input-container">
+            <label htmlFor="name" className="patient__input-label">
+              Name
+            </label>
+            <input
+              className="patient__input-text"
+              type="text"
+              id="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+          </div>
+          {/* IMAGE INPUT */}
+          <div className="patient__input-container">
+            <label htmlFor="image" className="patient__input-label">
+              Upload Image
+            </label>
+            <input
+              className="patient__input-image"
+              type="file"
+              id="image"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+          </div>
+
+          <button onClick={handleSubmit} className="patient__input-button">
+            Submit
+          </button>
         </div>
-
-        <div>
-          <p>Date</p>
-          <input placeholder="Insert Date" />
+        <div className="patient__right-container">
+          {form.image && (
+            <img
+              src={URL.createObjectURL(form.image)}
+              alt="Preview"
+              style={{ maxHeight: "100%", borderRadius: "3%" }}
+            />
+          )}
         </div>
-
-        <div>
-          <p>Note</p>
-          <input placeholder="Insert Note" />
-        </div>
-        
       </div>
 
+      {/* RESULTS */}
+      <div className="patient__bottom-container">
+        {results.map((result, index) => (
+          <ResultCard
+            key={index}
+            name={result.name}
+            testResult={result.testResult}
+            confidence={result.confidence}
+          />
+        ))}
+      </div>
     </div>
   );
 }
